7a0d8b5544333bfdb4288da21b6de77fd5825258
marvelopers <marvelopers@gmail.com>
2021-10-03 22:35:05 +0900

    소수 뒤집기

--- Algorithms & Data Structures/06_BruteForce/reversedDecimalCheck.js
+++ Algorithms & Data Structures/06_BruteForce/reversedDecimalCheck.js
@@ -1,4 +1,12 @@
 const numbers= [23,32,55,33,54,370,100];
+
+const isPrime=(num)=>{
+  if(num===1)return false;
+  for(let i=2; i<=num/2; i++){
+
+  }
+}
+
 const reversedDecimalCheck =(arr)=>{
   const answer;
 
@@ -9,6 +17,7 @@ const reversedDecimalCheck =(arr)=>{
       res = res*10+t;
       x=parseInt(x/10);
     }
+    if(isPrime(res))answer.push(res);
   }
   return answer;
 }
