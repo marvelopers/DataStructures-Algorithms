1d2b84d9d3fd25beaad8f1ab307bcd52d79f323e
marvelopers <marvelopers@gmail.com>
2021-10-21 18:54:30 +0900

    이분검색

---
+++ Algorithms & Data Structures/09_BinarySearch/binary.js
@@ -0,0 +1,20 @@
+let arr = [23,87,65,12,57,32,99,81];
+
+const binarySearch = (target, arr) => {
+  let answer ;
+  arr.sort((a,b)=>a-b);
+
+  let lt = 0;
+  let rt =arr.length-1;
+
+  while(lt<=rt){
+    let mid = parseInt((lt+rt)/2);
+    if(arr[mid]===target){
+      answer=mid+1;
+      break;
+    }else if(arr[mid]>target) rt=mid-1;
+    else lt=mid+1;
+  }
+
+  return answer;
+}
