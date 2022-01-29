e77438ac73d2d531d12c8958ab9a57b3453570fa
marvelopers <marvelopers@gmail.com>
2022-01-29 09:55:34 +0900

    Multiple Pointers

---
+++ Algorithms & Data Structures Master/Problem Solving Mechanism/ Multiple Pointers/counterUniqueValues.js
@@ -0,0 +1,14 @@
+// 🥳
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+const counterUniqueValues = (arr) => {
+  if (arr.length === 0) return 0;
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+};
---
+++ Algorithms & Data Structures Master/Problem Solving Mechanism/ Multiple Pointers/zeroSum.js
@@ -0,0 +1,31 @@
+// 🤔
+// Time Complexity : O(n^2)
+// Space Complexity : O(1)
+const nZeroSum = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+};
+
+// 🥳
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+const zeroSum = (arr) => {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+};
