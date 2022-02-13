fd50033d3e5d6802df108c60e24a19661bccaabe
marvelopers <marvelopers@gmail.com>
2022-02-13 16:10:19 +0900

    SinglyLinkedList.pop

--- Algorithms & Data Structures Master/Data Structures/SingleLinkedLists/index.js
+++ Algorithms & Data Structures Master/Data Structures/SingleLinkedLists/index.js
@@ -30,8 +30,31 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  // traverse() {
+  //   let current = this.head;
+  //   while (current) {
+  //     console.log(current.val);
+  //     current = current.next;
+  //   }
+  // }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (condition.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    return current;
+  }
 }
 
 const list = new SinglyLinkedList();
 list.push("Hello");
 list.push("Good Bye");
+list.push("!");
