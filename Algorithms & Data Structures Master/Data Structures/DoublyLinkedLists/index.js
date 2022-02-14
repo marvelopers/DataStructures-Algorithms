242e23793a6d847d97291e278994ff5d2b754e17
marvelopers <marvelopers@gmail.com>
2022-02-14 22:37:32 +0900

    DoublyLinkedLists.push & DoublyLinkedLists.pop

--- Algorithms & Data Structures Master/Data Structures/DoublyLinkedLists/index.js
+++ Algorithms & Data Structures Master/Data Structures/DoublyLinkedLists/index.js
@@ -12,4 +12,32 @@ class DoublyLinkedLists {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+      this.length++;
+      return this;
+    }
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
 }
